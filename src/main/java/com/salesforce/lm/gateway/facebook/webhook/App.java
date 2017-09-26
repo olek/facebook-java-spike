45ecbe25e8821549af06af4485076d87190979b9
Olek Poplavsky <opoplavsky@salesforce.com>
2017-09-26 12:31:52 -0400

    Add relaxed binding and validations to properties.

--- src/main/java/com/salesforce/lm/gateway/facebook/webhook/App.java
+++ src/main/java/com/salesforce/lm/gateway/facebook/webhook/App.java
@@ -3,15 +3,9 @@ package com.salesforce.lm.gateway.facebook.webhook;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
-    @Bean
-    public AppProperties appProperties() {
-        return new AppProperties();
-    }
-
     public static void main(String[] args) {
         new App()
             .configure(new SpringApplicationBuilder(App.class))
--- src/main/java/com/salesforce/lm/gateway/facebook/webhook/AppProperties.java
+++ src/main/java/com/salesforce/lm/gateway/facebook/webhook/AppProperties.java
@@ -1,12 +1,23 @@
 package com.salesforce.lm.gateway.facebook.webhook;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
+
+@Component
+@ConfigurationProperties("fb.webhook")
+@Validated
 public class AppProperties {
-    @Value( "${foo:default-bar}" )
+    @NotNull
     private String foo;
 
     public String foo() {
         return foo;
+    }
+
+    public void setFoo(String value) {
+        this.foo = value;
     }
 }
